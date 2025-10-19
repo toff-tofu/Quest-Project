59d0e1290248262e68301a3f66786e9461b3d732
toff-tofu <e8623y@gmail.com>
2025-10-19 10:58:00 -0700

    Messed up idk what is happening
    this better work
    i need to revert changes ig

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,177 +1,184 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    public float maxSpeed;
-    public float MoveSpeed;
-    public float JumpHeight;
-    public Rigidbody2D body;
-    public float abilityPower;
-    public float acceleration;
-    public float groundDrag;
-    public float airDrag;
-    public float abilityDrag;
-//-------------------------------------------------------------------
+// public class Movement : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     public float maxSpeed;
+//     public float MoveSpeed;
+//     public float JumpHeight;
+//     public Rigidbody2D body;
+//     public float abilityPower;
+//     public float acceleration;
+//     public float groundDrag;
+//     public float airDrag;
+//     public float abilityDrag;
+// //-------------------------------------------------------------------
+//     private bool grounded = false;
+//     private bool leftHanging = false;
+//     private bool rightHanging = false;
+//     private float walljumpXVel = 0;
+//     private float jumpSub = 1.2f;
 
-    private bool grounded = false;
-    private bool leftHanging = false;
-    private bool rightHanging = false;
-    private float walljumpXVel = 0;
-    private float jumpSub = 1.2f;
-    private Vector2 oldVel;
-    private Vector3 oldPos;
-    private float oldYVel;
-    private Vector2 abilitySpeed = new Vector2(0,0);
-//-------------------------------------------------------------------
+//     private Vector2 oldVel;
+//     private Vector3 oldPos;
+//     private float oldYVel;
+//     private Vector2 abilitySpeed = new Vector2(0,0);
+//     void Start()
+//     {
+//         // jumpSub = MoveSpeed / 7;
+//     }
 
-    void Start()
-    {
-        oldYVel = body.velocity.y;
-    }
-    void Update()
-    {
-        CheckGrounded();
-        CheckHanging();
-        Jump();
-        Ability();
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Move();
-        ApplyForces();
-        // CapSpeed();
-        Die();
-    }
-    private void CheckGrounded()
-    {
-        RaycastHit2D[] leftCols = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position - new Vector3(0.4f, 0, 0),
-                                                        Vector2.down, 0.8f, LayerMask.GetMask("Block"));
-        RaycastHit2D[] rightCols = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position + new Vector3(0.4f, 0, 0),
-                                                        Vector2.down, 0.8f, LayerMask.GetMask("Block"));
-        grounded = leftCols.Length > 0 || rightCols.Length > 0;
-        if (grounded)
-        {
-            print("You Hit The Ground");
-        }
-    }
-    private void CheckHanging()
-    {
-        RaycastHit2D[] leftColsTop = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position + new Vector3(0, 0.5f, 0),
-                                                        Vector2.left, 0.6f, LayerMask.GetMask("Block"));
-        RaycastHit2D[] leftColsBottom = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position - new Vector3(0, 0.5f, 0),
-                                                        Vector2.left, 0.6f, LayerMask.GetMask("Block"));
-        RaycastHit2D[] rightColsTop = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position + new Vector3(0, 0.5f, 0),
-                                                        Vector2.right, 0.6f, LayerMask.GetMask("Block"));
-        RaycastHit2D[] rightColsBottom = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position - new Vector3(0, 0.5f, 0),
-                                                        Vector2.right, 0.6f, LayerMask.GetMask("Block"));
-        leftHanging = leftColsTop.Length > 0 || leftColsBottom.Length > 0;
-        rightHanging = rightColsTop.Length > 0 || rightColsBottom.Length > 0;
-        if (grounded)
-        {
-            leftHanging = false;
-            rightHanging = false;
-        }
-    }
+//     void Update()
+//     {
+//         CheckGrounded();
+//         CheckHanging();
+//         Jump();
+//         Ability();
+//     }
+//     // Update is called once per frame
+//     void FixedUpdate()
+//     {
+//         Move();
+//         // CapSpeed();
+//         if (grounded)
+//         {
+//             leftHanging = false;
+//             rightHanging = false;
+//         }
+//         if (Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
+//         {
+//             Jump();
+//         }
+//     }
 
-    
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow)){
-            if (grounded)
-            {
-                gameObject.GetComponent<ParticleSystem>().Emit(10);
-                float oldXVel = body.velocity.x;
-                body.velocity = new Vector3(oldXVel, JumpHeight, 0);
-                print("You Jumped Off The Ground");
-            }
-            if (leftHanging)
-            {
-                gameObject.GetComponent<ParticleSystem>().Emit(5);
-                float oldXVel = body.velocity.x;
-                body.velocity = new Vector3(oldXVel, JumpHeight, 0);
-                walljumpXVel = MoveSpeed;
-            }
-            if (rightHanging)
-            {
-                gameObject.GetComponent<ParticleSystem>().Emit(5);
-                float oldXVel = body.velocity.x;
-                body.velocity = new Vector3(oldXVel, JumpHeight, 0);
-                walljumpXVel = -MoveSpeed;
-            }
-        }
-    }
-    void Die(){
-        if (gameObject.GetComponent<Transform>().position.y < -5)
-        {
-            body.position = new Vector2(0,0);
-        }
-    }
-    void Ability(){
-        if (Input.GetKeyDown("x")){
-            print("You Pressed Space");
-            abilitySpeed = new Vector2(abilityPower*Input.GetAxisRaw("Horizontal"), abilityPower*Input.GetAxisRaw("Vertical"));
-        }   
-        
-    }
-    void CapSpeed(){
-        if (body.velocity.x >= maxSpeed)
-        {
-            // float oldYVel = body.velocity.y;
-            body.velocity = new Vector2(maxSpeed, oldYVel);
-        }
-        if (body.velocity.x <= -maxSpeed)
-        {
-            // float oldYVel = body.velocity.y;
-            body.velocity = new Vector2(-maxSpeed, oldYVel);
-        }
-        
-    }
-    void Move(){
-        //acceleration based
-        // float hVal = Input.GetAxisRaw("Horizontal") * MoveSpeed;
-        // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(hVal, 0));
-        //direction based
-        float hVal = 0;
-        if (walljumpXVel < 3 && walljumpXVel >= 0 || walljumpXVel > -3 && walljumpXVel <= 0)
-        {
-            hVal = Input.GetAxisRaw("Horizontal") * MoveSpeed;
-        }
-        oldYVel = body.velocity.y;
-        oldVel = body.velocity;
-        oldPos = gameObject.GetComponent<Transform>().position;
-        body.AddForce(new Vector2((hVal + walljumpXVel)*acceleration, oldYVel));
-        body.AddForce(abilitySpeed);
-        walljumpXVel /= jumpSub;
-        if (body.velocity != oldVel && grounded)
-        {
-            // gameObject.GetComponent<AudioSource>().Play();
-            // gameObject.GetComponent<ParticleSystem>().Emit(1);
-        }
-        if (body.velocity == oldVel && grounded)
-        {
-            // gameObject.GetComponent<AudioSource>().Stop();
-            // gameObject.GetComponent<ParticleSystem>().Emit(1);
-        }
-    }
-    void ApplyForces(){
-        abilitySpeed = new Vector2(abilitySpeed.x/abilityDrag, abilitySpeed.y/abilityDrag);
-        if (abilitySpeed.x < 0.5f && abilitySpeed.x > -0.5f){
-            abilitySpeed = new Vector2(0,abilitySpeed.y);
-        }
-        if (abilitySpeed.y < 0.5f && abilitySpeed.y > -0.5f){
-            abilitySpeed = new Vector2(abilitySpeed.x,0);
-        }
-        // if (grounded){
-        //     body.drag = groundDrag;
-        // }
-        // if (body.velocity.y>0){
-        //     body.AddForce(airDrag*Vector2.down);
-        // }
-        // if (body.velocity.x!=0&&!grounded){
-        //     body.AddForce(new Vector2(-Mathf.Sign(body.velocity.x)*airDrag,0));
-        // }
-    }
-}
+//     private void CheckGrounded()
+//     {
+//         RaycastHit2D[] leftCols = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position - new Vector3(0.4f, 0, 0),
+//                                                         Vector2.down, 0.8f, LayerMask.GetMask("Block"));
+//         RaycastHit2D[] rightCols = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position + new Vector3(0.4f, 0, 0),
+//                                                         Vector2.down, 0.8f, LayerMask.GetMask("Block"));
+//         grounded = leftCols.Length > 0 || rightCols.Length > 0;
+//         if (grounded)
+//         {
+//             print("You Hit The Ground");
+//         }
+//     }
+//     private void CheckHanging()
+//     {
+//         RaycastHit2D[] leftColsTop = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position + new Vector3(0, 0.5f, 0),
+//                                                         Vector2.left, 0.6f, LayerMask.GetMask("Block"));
+//         RaycastHit2D[] leftColsBottom = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position - new Vector3(0, 0.5f, 0),
+//                                                         Vector2.left, 0.6f, LayerMask.GetMask("Block"));
+//         RaycastHit2D[] rightColsTop = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position + new Vector3(0, 0.5f, 0),
+//                                                         Vector2.right, 0.6f, LayerMask.GetMask("Block"));
+//         RaycastHit2D[] rightColsBottom = Physics2D.RaycastAll(gameObject.GetComponent<Transform>().position - new Vector3(0, 0.5f, 0),
+//                                                         Vector2.right, 0.6f, LayerMask.GetMask("Block"));
+//         leftHanging = leftColsTop.Length > 0 || leftColsBottom.Length > 0;
+//         rightHanging = rightColsTop.Length > 0 || rightColsBottom.Length > 0;
+//     }
+
+//     void Jump()
+//     {
+//         if (grounded)
+//         {
+//             gameObject.GetComponent<ParticleSystem>().Emit(10);
+//             float oldXVel = gameObject.GetComponent<Rigidbody2D>().velocity.x;
+//             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(oldXVel, JumpHeight, 0);
+//             print("You Jumped Off The Ground");
+//         }
+//     }
+//     void Ability(){
+//         if (Input.GetKeyDown("x")){
+//             print("You Pressed Space");
+//             abilitySpeed = new Vector2(abilityPower*Input.GetAxisRaw("Horizontal"), abilityPower*Input.GetAxisRaw("Vertical"));
+//         }   
+
+//     }
+//     void CapSpeed(){
+//         if (body.velocity.x >= maxSpeed)
+//         if (leftHanging)
+//         {
+//             gameObject.GetComponent<ParticleSystem>().Emit(5);
+//             float oldXVel = gameObject.GetComponent<Rigidbody2D>().velocity.x;
+//             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(oldXVel, JumpHeight, 0);
+//             // jumpXVel = MoveSpeed;
+//         }
+//         if (rightHanging)
+//         {
+//             gameObject.GetComponent<ParticleSystem>().Emit(5);
+//             float oldXVel = gameObject.GetComponent<Rigidbody2D>().velocity.x;
+//             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(oldXVel, JumpHeight, 0);
+//             // jumpXVel = -MoveSpeed;
+//         }
+//     }
+//     void Move(){
+//     // Update is called once per frame
+//     // void FixedUpdate()
+//     // {
+//     //     //acceleration based
+//     //     // float hVal = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+//     //     // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(hVal, 0));
+//     //     //direction based
+//     //     float hVal = 0;
+//     //     if (jumpXVel < 3 && jumpXVel >= 0 || jumpXVel > -3 && jumpXVel <= 0)
+//     //     {
+//     //         hVal = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+//     //     }
+//     //     oldYVel = body.velocity.y;
+//     //     oldVel = body.velocity;
+//     //     oldPos = gameObject.GetComponent<Transform>().position;
+//     //     body.AddForce(new Vector2((hVal + walljumpXVel)*acceleration, oldYVel));
+//     //     body.AddForce(abilitySpeed);
+//     //     walljumpXVel /= jumpSub;
+//     //     if (body.velocity != oldVel && grounded)
+//     //     float oldYVel = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+//     //     Vector2 oldVel = gameObject.GetComponent<Rigidbody2D>().velocity;
+//     //     Vector3 oldPos = gameObject.GetComponent<Transform>().position;
+//     //     gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(hVal + jumpXVel, oldYVel);
+//     //     jumpXVel /= jumpSub;
+
+//     //     if (gameObject.GetComponent<Rigidbody2D>().velocity != oldVel && grounded)
+//     //     {
+//     //         // gameObject.GetComponent<AudioSource>().Play();
+//     //         // gameObject.GetComponent<ParticleSystem>().Emit(1);
+//     //     }
+//     //     if (gameObject.GetComponent<Rigidbody2D>().velocity == oldVel && grounded)
+//     //     {
+//     //         // gameObject.GetComponent<AudioSource>().Stop();
+//     //         // gameObject.GetComponent<ParticleSystem>().Emit(1);
+//     //     }
+//     // }
+//     void ApplyForces(){
+//         abilitySpeed = new Vector2(abilitySpeed.x/abilityDrag, abilitySpeed.y/abilityDrag);
+//         if (abilitySpeed.x < 0.5f && abilitySpeed.x > -0.5f){
+//             abilitySpeed = new Vector2(0,abilitySpeed.y);
+//         if (gameObject.GetComponent<Rigidbody2D>().velocity.x >= maxSpeed)
+//         {
+//             // float oldYVel = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+//             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(maxSpeed, oldYVel);
+//         }
+//         if (gameObject.GetComponent<Rigidbody2D>().velocity.x <= -maxSpeed)
+//         {
+//             // float oldYVel = gameObject.GetComponent<Rigidbody2D>().velocity.y;
+//             gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-maxSpeed, oldYVel);
+//         }
+//         if (gameObject.GetComponent<Transform>().position.y < -5)
+//         {
+//             gameObject.GetComponent<Rigidbody2D>().position = new Vector2(0,0);
+//         }
+//         if (abilitySpeed.y < 0.5f && abilitySpeed.y > -0.5f){
+//             abilitySpeed = new Vector2(abilitySpeed.x,0);
+//         }
+//         // if (grounded){
+//         //     body.drag = groundDrag;
+//         // }
+//         // if (body.velocity.y>0){
+//         //     body.AddForce(airDrag*Vector2.down);
+//         // }
+//         // if (body.velocity.x!=0&&!grounded){
+//         //     body.AddForce(new Vector2(-Mathf.Sign(body.velocity.x)*airDrag,0));
+//         // }
+//     }
+// }
