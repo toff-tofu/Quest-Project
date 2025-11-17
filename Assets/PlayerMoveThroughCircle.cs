d25837ee951d0c30307f28141edc661d7e6bc350
toff-tofu <e8623y@gmail.com>
2025-11-16 20:11:47 -0800

    idk grapple

--- Assets/PlayerMoveThroughCircle.cs
+++ Assets/PlayerMoveThroughCircle.cs
@@ -9,44 +9,70 @@ public class PlayerMoveThroughCircle : MonoBehaviour
     private Rigidbody2D playerBody;
     private bool _insideCircle = false;
     private Vector2 startPosition;
-    private bool _moving = false;
+    [SerializeField] private AreaEffector2D _areaEffector;
+    private float _pxdrag;
+    private float _pgravity;
+    [SerializeField] private CircleCollider2D hitBox;
+    [SerializeField] private LayerMask playerMask;
     void Start()
     {
         playerBody = player.GetComponent<Rigidbody2D>();
+        _pxdrag = player.GetComponent<Movement>().xDrag;
+        _pgravity = playerBody.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if (_insideCircle) { preIn = true; } else { preIn = false; }
+        CheckCollisions();
+
         if (_insideCircle)
         {
+            _areaEffector.forceAngle = Mathf.Atan2(startPosition.y - transform.position.y, startPosition.x - transform.position.x) * Mathf.Rad2Deg;
+            // transform.LookAt(new Vector2(startPosition.x, startPosition.y));
             if (Input.GetKey("c"))
             {
-                _moving = true;
+                _areaEffector.forceMagnitude = -200f;
+                player.GetComponent<Movement>().xDrag = 0f;
+                playerBody.gravityScale = 0f;
             }
-            if (_moving == true)
+            else
             {
-                Vector2 toCenter = new Vector2(transform.position.x - startPosition.x,
-                                               transform.position.y - startPosition.y).normalized;
-                playerBody.AddForce(toCenter * GetComponent<CircleCollider2D>().radius * 50);
+                _areaEffector.forceMagnitude = 0f;
+                player.GetComponent<Movement>().xDrag = _pxdrag;
+                playerBody.gravityScale = _pgravity;
             }
         }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        else
         {
-            _insideCircle = true;
-            startPosition = playerBody.transform.position;
+            _areaEffector.forceMagnitude = 0f;
+            player.GetComponent<Movement>().xDrag = _pxdrag;
+            playerBody.gravityScale = _pgravity;
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
+    void CheckCollisions()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        Collider2D[] overlaps = Physics2D.OverlapAreaAll(hitBox.bounds.min, hitBox.bounds.max, playerMask);
+        if (overlaps.Length == 0)
         {
             _insideCircle = false;
-            _moving = false;
-            // startPosition = Vector2.zero;
+            // if (preIn == false)
+            // {
+            //     startPosition = player.transform.position;
+            //     preIn = true;
+            // }
+        }
+        else
+        {
+            _insideCircle = true;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            startPosition = player.transform.position;
         }
     }
 }
